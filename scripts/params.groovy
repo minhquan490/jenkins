a76b5abb366ebfa38e07d7103c6a418e89d6448e
quan <behom4901@gmail.com>
2024-09-19 11:28:13 +0700

    JEN-fix: add scripts

--- scripts/params.groovy
+++ scripts/params.groovy
@@ -1,10 +1,9 @@
 listView('Example View') {
     description('This is a list view created from a DSL script in Git')
     jobs {
-        name('build-job')
+
     }
     columns {
-        buildParameters()
         status()
         weather()
         name()
@@ -12,18 +11,5 @@ listView('Example View') {
         lastFailure()
         lastDuration()
         buildButton()
-    }
-}
-
-job('build-job') {
-    description('This is a build job created from Job DSL')
-    scm {
-        git('https://github.com/minhquan490/jenkins.git', 'master')
-    }
-    steps {
-        shell('echo "Running build script..."')
-    }
-    environmentVariables {
-        env('jdbc-url', 'url')
     }
 }
