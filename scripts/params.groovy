a485a104fcefc64e53966d71cb27a55e4ccc4008
quan <behom4901@gmail.com>
2024-09-19 10:57:34 +0700

    JEN-fix: add scripts

--- scripts/params.groovy
+++ scripts/params.groovy
@@ -1,10 +1,7 @@
-// Example DSL script located in: scripts/job-dsl.groovy
-
 listView('Example View') {
     description('This is a list view created from a DSL script in Git')
     jobs {
         name('build-job')
-        name('test-job')
     }
     columns {
         status()
@@ -20,22 +17,12 @@ listView('Example View') {
 job('build-job') {
     description('This is a build job created from Job DSL')
     scm {
-        git('https://github.com/your-repo/build-job.git', 'main')
-    }
-    triggers {
-        scm('H/15 * * * *')
+        git('https://github.com/minhquan490/jenkins.git', 'master')
     }
     steps {
         shell('echo "Running build script..."')
     }
-}
-
-job('test-job') {
-    description('This is a test job created from Job DSL')
-    scm {
-        git('https://github.com/your-repo/test-job.git', 'main')
-    }
-    steps {
-        shell('echo "Running test script..."')
+    environmentVariables {
+        env('jdbc-url', 'url')
     }
 }
